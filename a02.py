ca1384756ec4f81e1ed403cdfba64b96ad2c0f6f
Muhammad Ahsan <p176142@nu.edu.pk>
2018-03-08 09:59:24 -0800

    Second Assignment

---
+++ a02.py
@@ -0,0 +1,31 @@
+## IMPORTS GO HERE
+from math import pi
+## END OF IMPORTS 
+
+
+
+
+#### YOUR CODE FOR get_area GOES HERE ####
+def get_area(radius):
+	r=radius
+	area=pi*pow(radius,2)
+	return area
+#### End OF MARKER
+
+
+
+
+#### YOUR CODE FOR output_parameter GOES HERE ####
+def output_parameter(radius):
+	r=radius
+	parameter=2*pi*radius
+	print 'The parameter of the circle with radius', r ,'is',parameter
+#### End OF MARKER  
+
+
+
+
+if __name__ == '__main__': 
+    print get_area(2) 
+    output_parameter(1.0) 
+
